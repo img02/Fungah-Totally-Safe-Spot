fa38e61bf3dff32a1aec3a807f98a028d0ea1d09
Will <70348218+img02@users.noreply.github.com>
2024-06-01 15:47:15 +1000

    enable npc check

--- Fungah/PluginUI.cs
+++ Fungah/PluginUI.cs
@@ -117,8 +117,7 @@ namespace SamplePlugin
         /// <returns></returns>
         private bool PlayerOnStage()
         {
-            //if (_clientState.LocalPlayer == null || !PlayerAtGoldSaucer() || !IsFungahEvent()) return false;
-            if (_clientState.LocalPlayer == null || !PlayerAtGoldSaucer()) return false;           
+            if (_clientState.LocalPlayer == null || !PlayerAtGoldSaucer() || !IsFungahEvent()) return false;            
             var pos = _clientState.LocalPlayer.Position;
             // south and north are negative values.
             return ((pos.X is > StageWest and < StageEast) && (pos.Z is < StageSouth and > StageNorth));
